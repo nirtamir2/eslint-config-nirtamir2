316a50143b8701af4009d616ac09d44e578bd2ec
Nir Tamir <nirtamir2@gmail.com>
2022-08-20 16:18:57 +0300

    slow rules warning

--- slow-rules.js
+++ slow-rules.js
@@ -1,7 +1,7 @@
 // @see https://typescript-eslint.io/docs/linting/troubleshooting#eslint-plugin-import
 module.exports = {
   rules: {
-    "import/no-cycle": 2,
-    "import/no-unused-modules": 2,
+    "import/no-cycle": 1,
+    "import/no-unused-modules": [1, { unusedExports: true }],
   },
 };
