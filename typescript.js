056ed6029bd5796e043db38885a7a2b732be55cd
Nir Tamir <nirtamir2@gmail.com>
2023-04-06 23:13:23 +0300

    change deprecated rule

--- typescript.js
+++ typescript.js
@@ -268,7 +268,7 @@ module.exports = {
     "@typescript-eslint/no-non-null-asserted-nullish-coalescing": "error",
     "@typescript-eslint/no-non-null-asserted-optional-chain": "error",
     "@typescript-eslint/no-non-null-assertion": "error",
-    "@typescript-eslint/no-parameter-properties": "error",
+    "@typescript-eslint/parameter-properties": "error",
     "no-redeclare": "off",
     "@typescript-eslint/no-redeclare": "error",
     "@typescript-eslint/no-require-imports": "error",
