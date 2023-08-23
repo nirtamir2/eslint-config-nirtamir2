50cb28bfb6f871ab25dbf819947fdc5ffe43bbbf
Nir Tamir <nirtamir2@gmail.com>
2023-08-23 22:31:04 +0300

    i18n

--- i18n.js
+++ i18n.js
@@ -1,19 +1,55 @@
 const path = require("node:path");
 
 module.exports = {
-  // When using it - we need to specify that and wrap with override
-  // files: ["**/locales/**/*.json"],
-  extends: ["plugin:i18n-json/recommended"],
+  extends: ["plugin:i18n-prefix/recommended", "plugin:i18next/recommended"],
+  plugins: ["@naturacosmeticos/i18n-checker"],
   rules: {
-    "i18n-json/valid-message-syntax": [
+    /**
+     * This will error if `t` function called with a translation key
+     * that does not exist in translation files
+     */
+    "@naturacosmeticos/i18n-checker/path-in-locales": [
       "error",
       {
-        /**
-         * This allow to use the i18next format with {param} syntax
-         * @see https://github.com/godaddy/eslint-plugin-i18n-json/issues/40#issuecomment-842474651
-         */
-        syntax: path.resolve("./_i18next-syntax-validator.js"),
+        localesPath: "locales/",
+        messagesBasePath: "translations",
+        translationFunctionName: "t",
+      },
+    ],
+    /**
+     * i18n translation should be the same as component name
+     */
+    "i18n-prefix/i18n-prefix": [
+      "error",
+      {
+        translationFunctionName: "t",
+        delimiter: ".",
+        ignorePrefixes: ["enum"],
       },
     ],
   },
+  overrides: [
+    {
+      files: ["**/locales/**/*.json"],
+      extends: ["plugin:i18n-json/recommended"],
+      rules: {
+        "i18n-json/valid-message-syntax": [
+          "error",
+          {
+            /**
+             * This allows using the i18next format with {param} syntax
+             * @see https://github.com/godaddy/eslint-plugin-i18n-json/issues/40#issuecomment-842474651
+             */
+            syntax: path.resolve("./_i18next-syntax-validator.js"),
+          },
+        ],
+      },
+    },
+    {
+      files: ["**.stories.tsx"],
+      rules: {
+        "i18next/no-string-literal": "off",
+      },
+    },
+  ],
 };
